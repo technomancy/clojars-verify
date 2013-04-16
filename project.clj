daa26b39341b6d9dae54269ca0ac127eb0bb90c8
Phil Hagelberg <technomancy@gmail.com>
2013-04-16 14:48:24 -0700

    Shell out to sha1sum.
    For some reason the commons-digest checksums never match the index.

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :description "Verify checksums of Clojars artifacts"
   :license "Eclipse Public License 1.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clucy "0.3.0"]
-                 [commons-codec "1.6"]]
+                 [clucy "0.3.0"]]
   :min-lein-version "2.0.0")
--- src/clojars/verify.clj
+++ src/clojars/verify.clj
@@ -1,18 +1,26 @@
 (ns clojars.verify
   (:require [clucy.core :as c]
             [clojure.java.io :as io]
-            [clojure.string :as s])
-  (:import (org.apache.commons.codec.digest DigestUtils)
-           (java.util.zip ZipFile ZipEntry)))
+            [clojure.string :as s]
+            [clojure.java.shell :as sh])
+  (:import (java.util.zip ZipFile ZipEntry)))
 
 (def jar-pattern #"/([^/]+)/([^/]+)/([^/]+)/([^/]+)\.jar$")
 
 (defn query-for [coords]
   (apply format "g:%s AND a:%s AND v:%s" coords))
 
+(defn sha [x]
+  (if (instance? java.io.File x)
+    (first (.split (:out (sh/sh "sha1sum" (str x))) " "))
+    (let [tmp (doto (java.io.File/createTempFile "clojars" "verify")
+                (.deleteOnExit))]
+      (io/copy x tmp)
+      (recur tmp))))
+
 (defn jar-mismatch? [doc f]
   (let [from-index (:1 doc)
-        from-repo (DigestUtils/shaHex (io/input-stream f))]
+        from-repo (sha (io/input-stream f))]
     (if (not= from-index from-repo)
       (format "jar mismatch, index: %s repo: %s" from-index from-repo))))
 
@@ -20,11 +28,13 @@
   (let [pom-entry-name (apply format "META-INF/maven/%s/%s/pom.xml" coords)
         pom-file-name (s/replace (str f) #"\.jar" ".pom")
         z (ZipFile. f)
-        entry (.getEntry z pom-entry-name)
-        from-jar (DigestUtils/shaHex (.getInputStream z entry))
-        from-repo (DigestUtils/shaHex (io/input-stream f))]
-    (if (not= from-jar from-repo)
-      (format "pom mismatch, jar: %s repo %s" from-jar from-repo))))
+        entry (.getEntry z pom-entry-name)]
+    (if entry
+      (let [from-jar (sha (.getInputStream z entry))
+            from-repo (sha (io/input-stream f))]
+        (if (not= from-jar from-repo)
+          (format "pom mismatch, jar: %s repo %s" from-jar from-repo)))
+      "no pom in jar")))
 
 (defn -main [index-location repository & [limit]]
   (with-open [index (c/disk-index index-location)]
